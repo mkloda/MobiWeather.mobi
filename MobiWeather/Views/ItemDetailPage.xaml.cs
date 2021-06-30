1c186762d80b7902d126b963547a313dfc6b4418
Mikołaj Kłoda <mikolajkloda@hotmail.com>
2021-06-30 12:55:27 +0200

    Dodaj pliki projektów.

---
+++ MobiWeather/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MobiWeather.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MobiWeather.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
